bdab4f02b5a7b4edbb37431f279826dd7fa579b1
dongk00 <dddongk00@gmail.com>
2023-06-02 14:15:03 +0900

    Clean Architecture 구조 생성

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,30 @@
+object AndroidX {
+    const val CORE = "androidx.core:core-ktx:1.10.0"
+    const val LIFECYCLE_RUNTIME = "androidx.lifecycle:lifecycle-runtime-ktx:2.6.1"
+    const val COMPOSE_ACTIVITY = "androidx.activity:activity-compose:1.7.1"
+    const val COMPOSE_UI = "androidx.compose.ui:ui:1.4.3"
+    const val COMPOSE_UI_TOOLING_PREVIEW = "androidx.compose.ui:ui-tooling-preview:1.4.3"
+    const val COMPOSE_MATERIAL = "androidx.compose.material:material:1.4.3"
+    const val COMPOSE_UI_TOOLING = "androidx.compose.ui:ui-tooling:1.4.3"
+}
+
+object Google {
+    const val MATERIAL = "com.google.android.material:material:1.8.0"
+    const val HILT = "com.google.dagger:hilt-android:2.44"
+    const val HILT_COMPILER = "com.google.dagger:hilt-android-compiler:2.44"
+}
+
+object SquareUp {
+    const val OKHTTP3 = "com.squareup.okhttp3:okhttp:4.9.1"
+    const val OKHTTP3_INTERCEPTOR = "com.squareup.okhttp3:logging-interceptor:4.9.1"
+    const val RETROFIT2 = "com.squareup.retrofit2:retrofit:2.9.0"
+    const val GSON_CONVERTER = "com.squareup.retrofit2:converter-gson:2.9.0"
+}
+
+object AndroidTest {
+    const val JUNIT4: String = "junit:junit:4.13.2"
+    const val EXT_JUNIT = "androidx.test.ext:junit:1.1.5"
+    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.5.1"
+    const val COMPOSE_JUNIT4 = "androidx.compose.ui:ui-test-junit4:1.4.3"
+    const val COMPOSE_UI_MANIFEST = "androidx.compose.ui:ui-test-manifest:1.4.3"
+}
